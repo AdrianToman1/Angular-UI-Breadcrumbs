666ff564d7a92e9f57bf4a872eff25991042dea3
Adrian Toman <adrian@adriantoman.com>
2017-01-08 15:50:24 +1100

    Setup unit testing using jasime and karma.
    Setup unit testing using jasime and karma. Including in Gulp build.
    Needed to include third party libraries to get test to run. All test are
    successful.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify'),
     gutil = require('gulp-util'),
-    eslint = require('gulp-eslint');
+    eslint = require('gulp-eslint'),
+    karma = require('karma');
 
 gulp.task('lint', function(){
   return gulp.src(['uiBreadcrumbs.js'])
@@ -11,7 +12,14 @@ gulp.task('lint', function(){
   .pipe(eslint.failAfterError());
 });
 
-gulp.task('default',  ['lint'], function(){
+gulp.task('test', function (done) {
+  new karma.Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
+
+gulp.task('default',  ['lint', 'test'], function(){
   return gulp.src(['uiBreadcrumbs.js'])
   .on('error', console.log)
   .pipe(uglify().on('error', gutil.log))
